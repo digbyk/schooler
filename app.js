d402a7858bb3cdede1f28b39c18d675d9b8d3622
digbyk <digby@digby.net>
2015-09-18 23:03:26 +0100

    Tidying

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').load();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -9,13 +10,11 @@ var passport = require('passport');
 
 var db = require('./model');
 
-var app = express();
-
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 
-// all environments
-//app.set('port', process.env.PORT || 3000);
+var app = express();
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -36,17 +35,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/index')(app, passport);
-require('./routes/home')(app, passport);
-require('./routes/gifts')(app);
-require('./routes/lists')(app);
-require('./routes/api')(app);
 
 app.use(function (req, res, next) {
 	res.status(404).send('Sorry cant find that!');
 });
 
-app.set('port', process.env.PORT || 3000);
+var host = process.env.HOST || '127.0.0.1';
+var port = process.env.PORT || 3000;
 
-var server = app.listen(app.get('port'), function () {
-	console.log('Express server listening on port ' + server.address().port);
+app.listen(port, host, function () {
+	console.log("Listening on " + host + ":" + port)
 });
